b24921f6c8e6258bf28a3e2e21cd2578483956c8
luckybo0027 <luckybo0027@gmail.com>
2023-02-09 16:45:20 +0800

    02-项目准备-vw适配方案

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 375
+    }
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="box">
-    <van-button type="primary">点我</van-button>
-    <van-cell>123</van-cell>
-  </div>
+  <div class="box"></div>
 </template>
 
 <style lang="scss" scoped></style>
