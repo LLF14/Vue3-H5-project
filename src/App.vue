060534bf31ff7ee3b9135af4e11ace26827fe303
luckybo0027 <luckybo0027@gmail.com>
2023-02-09 17:14:02 +0800

    03-项目准备-修改vant主题色

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="box"></div>
+  <div class="box">
+    <VanButton type="primary">给你的color 看看</VanButton>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
