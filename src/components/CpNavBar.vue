d8dea53e55c12dcaededdeb9356ec91ee4116f66
luckybo0027 <luckybo0027@gmail.com>
2023-02-09 18:06:15 +0800

    01-组件封装-CpNavBar-静态结构

---
+++ src/components/CpNavBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+/*
+  学习目标：cpNavbar组件封装 - 静态结构和代码解读
+  步骤：
+     1. 
+     2. 
+     3. 
+*/
+</script>
+
+<template>
+  <van-nav-bar left-arrow fixed placeholder title="注册" right-text="注册" />
+</template>
+
+<style lang="scss" scoped>
+::v-deep() {
+  .van-nav-bar {
+    &__arrow {
+      font-size: 18px;
+      color: var(--cp-text1);
+    }
+    &__text {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>登录页面</div>
+  <div>
+    <!-- 无需手动导入组件， 插件会自动导入vant vue-router 和 components下的全局组件 -->
+    <CpNavBar />
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
