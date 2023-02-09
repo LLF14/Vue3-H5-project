5b1249a9aa0c6b562de18f7516526bf2baf76ab1
luckybo0027 <luckybo0027@gmail.com>
2023-02-09 16:12:32 +0800

    01-项目准备-安装vant和自动按需加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="box">App</div>
+  <div class="box">
+    <van-button type="primary">点我</van-button>
+    <van-cell>123</van-cell>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 💥💥 全局导入vant样式文件， 注意放在main.scss导入顺序之前
+import 'vant/lib/index.css'
 
 import '@/styles/main.scss'
 
+// 💥💥 修复vant组件没有提示的问题
+import {} from 'vant'
 const app = createApp(App)
 
 app.use(createPinia())
